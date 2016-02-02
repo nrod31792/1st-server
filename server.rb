51c6af4ccd929761f29b1daff56a4378a74b04a8
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-02-02 16:11:14 -0500

    hw attempt 1

--- server.rb
+++ server.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
+require 'hello.txt'
 
-get '/' do
-  "Hello World"
+get '//' do
+  puts File.read(hello.txt)
+end
+
 end
 
 get '/blah' do
   "Hello World"
+end
+
+get '//' do
+
+
 end
